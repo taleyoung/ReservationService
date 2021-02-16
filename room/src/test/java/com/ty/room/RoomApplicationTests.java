63642e413271a4cf1dd77ec391da628155fa1773
taleyoung <1090117853@qq.com>
2021-02-16 14:15:54 +0800

    feat: ❤️ initialize project common & room

---
+++ room/src/test/java/com/ty/room/RoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
